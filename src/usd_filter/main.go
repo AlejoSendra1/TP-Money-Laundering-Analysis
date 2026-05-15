dbd8247baa30b024d3e7996a46add23bd743a149
4cea2 <kcarbajal@fi.uba.ar>
2026-05-15 11:57:29 -0300

    feat: implement USD Filter

---
+++ src/usd_filter/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"log/slog"
+	"os"
+	"strconv"
+
+	"github.com/AlejoSendra1/TP-Money-Laundering-Analysis/usd_filter"
+)
+
+func loadConfig() (usd_filter.USDFilterConfig, error) {
+	// TODO: Agregar ID al dockerfile?
+	id, err := strconv.Atoi(os.Getenv("ID"))
+	if err != nil {
+		return usd_filter.USDFilterConfig{}, err
+	}
+
+	// TODO: Agregar MOM_PORT al dockerfile?
+	momPort, err := strconv.Atoi(os.Getenv("MOM_PORT"))
+	if err != nil {
+		return usd_filter.USDFilterConfig{}, errors.New("MOM_PORT environment variable is required and must be a number")
+	}
+
+	momHost := os.Getenv("MOM_HOST")
+	if momHost == "" {
+		return usd_filter.USDFilterConfig{}, errors.New("MOM_HOST environment variable is required")
+	}
+
+	inputQueue := os.Getenv("INPUT_QUEUE")
+	if inputQueue == "" {
+		return usd_filter.USDFilterConfig{}, errors.New("INPUT_QUEUE environment variable is required")
+	}
+
+	inputTopic := os.Getenv("INPUT_TOPIC")
+	if inputTopic == "" {
+		return usd_filter.USDFilterConfig{}, errors.New("INPUT_TOPIC environment variable is required")
+	}
+
+	outputTopic := os.Getenv("OUTPUT_TOPIC")
+	if outputTopic == "" {
+		return usd_filter.USDFilterConfig{}, errors.New("OUTPUT_TOPIC environment variable is required")
+	}
+
+	return usd_filter.USDFilterConfig{
+		Id:          id,
+		MomHost:     momHost,
+		MomPort:     momPort,
+		InputQueue:  inputQueue,
+		InputTopic:  inputTopic,
+		OutputTopic: outputTopic,
+	}, nil
+}
+
+func run() int {
+	config, err := loadConfig()
+	if err != nil {
+		slog.Error("While loading config", "err", err)
+		return 1
+	}
+
+	server, err := usdFilter.NewUSDFilter(config)
+	if err != nil {
+		slog.Error("While initializing usd filter", "err", err)
+		return 1
+	}
+
+	server.Run()
+	return 0
+}
+
+func main() {
+	os.Exit(run())
+}
---
+++ src/usd_filter/usd_filter/usd_filter.go
@@ -0,0 +1,108 @@
+package usd_filter
+
+import (
+	"log/slog"
+
+	"github.com/AlejoSendra1/TP-Money-Laundering-Analysis/common/messageprotocol/inner"
+	"github.com/AlejoSendra1/TP-Money-Laundering-Analysis/common/middleware"
+	"github.com/AlejoSendra1/TP-Money-Laundering-Analysis/common/transaction"
+)
+
+const USDCurrencyName = "US Dollar"
+
+type USDFilterConfig struct {
+	Id          int
+	MomHost     string
+	MomPort     int
+	InputQueue  string
+	InputTopic  string
+	OutputTopic string
+}
+
+type USDFilter struct {
+	inputExchange  middleware.Middleware
+	outputExchange middleware.Middleware
+	config         USDFilterConfig
+}
+
+func NewUSDFilter(config USDFilterConfig) (*USDFilter, error) {
+	connSettings := middleware.ConnSettings{Hostname: config.MomHost, Port: config.MomPort}
+
+	inputExchange, err := middleware.CreateExchangeMiddleware(config.InputQueue, []string{config.InputTopic}, connSettings)
+	if err != nil {
+		return nil, err
+	}
+
+	outputExchange, err := middleware.CreateExchangeMiddleware(config.OutputTopic, []string{config.OutputTopic}, connSettings)
+	if err != nil {
+		inputExchange.Close()
+		return nil, err
+	}
+
+	return &USDFilter{
+		inputExchange:  inputExchange,
+		outputExchange: outputExchange,
+		config:         config,
+	}, nil
+}
+
+func (usdFilter *USDFilter) Run() {
+	usdFilter.inputExchange.StartConsuming(func(msg middleware.Message, ack, nack func()) {
+		usdFilter.handleMessage(msg, ack, nack)
+	})
+}
+
+func (usdFilter *USDFilter) handleMessage(msg middleware.Message, ack func(), nack func()) {
+	// TODO: Actualizar DeserializeMessage con transaction en lugar de FruitItem
+	transactionRecords, clientID, isEof, err := inner.DeserializeMessage(msg)
+	if err != nil {
+		slog.Error("While deserializing message", "err", err, "clientID", clientID)
+		nack()
+		return
+	}
+
+	if isEof {
+		if err := usdFilter.handleEndOfRecordMessage(clientID); err != nil {
+			slog.Error("While handling end of record message", "err", err, "clientID", clientID)
+			nack()
+			return
+		}
+		ack()
+		return
+	}
+	if err := usdFilter.handleDataMessage(transactionRecords, clientID); err != nil {
+		slog.Error("While handling data message", "err", err, "clientID", clientID)
+		nack()
+		return
+	}
+	ack()
+}
+
+func (usdFilter *USDFilter) handleEndOfRecordMessage(clientID string) error {
+	return usdFilter.sendOutput([]transaction.Transaction{}, clientID)
+}
+
+func (usdFilter *USDFilter) handleDataMessage(transactionRecords []transaction.Transaction, clientID string) error {
+	for _, transactionRecord := range transactionRecords {
+		if transactionRecord.Currency == USDCurrencyName {
+			if err := usdFilter.sendOutput([]transaction.Transaction{transactionRecord}, clientID); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (usdFilter *USDFilter) sendOutput(transactionRecords []transaction.Transaction, clientID string) error {
+	// TODO: Actualizar SerializeMessage con transaction en lugar de FruitItem
+	message, err := inner.SerializeMessage(transactionRecords, clientID)
+	if err != nil {
+		slog.Debug("While serializing data message", "err", err, "clientID", clientID)
+		return err
+	}
+	if err := usdFilter.outputExchange.Send(*message); err != nil {
+		slog.Debug("While sending data message", "err", err, "clientID", clientID)
+		return err
+	}
+	return nil
+}
