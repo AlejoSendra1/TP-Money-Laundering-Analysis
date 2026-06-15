0193d7ab54a41b45e11b095bc22ab0b9a8053294
4cea2 <kcarbajal@fi.uba.ar>
2026-06-15 03:59:02 -0300

    feat(date_filter): add id to eof sender

--- src/date_filter/date_filter/date_filter.go
+++ src/date_filter/date_filter/date_filter.go
@@ -211,7 +211,7 @@ func (dateFilter *DateFilter) handleControlMessage(msg *middleware.Message, ack 
 		return
 	}
 
-	msgEOF, err := inner.SerializeEOF(clientID, true, "date_filter") // TO DO agregar otra var de entorno y para group tmb
+	msgEOF, err := inner.SerializeEOF(clientID, true, fmt.Sprintf("%d", dateFilter.config.Id)) // TO DO agregar otra var de entorno y para group tmb
 	if err != nil {
 		slog.Info("While serializing EOF message", "err", err, "clientID", clientID)
 		nack()
