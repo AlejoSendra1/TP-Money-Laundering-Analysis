cbb77e83b97e7edc44f8ea4b6cb0572ae0f2361b
Matias Rea <matias.rea@despegar.com>
2026-06-22 16:03:07 -0300

    feat: watchdog main

---
+++ src/watchdog/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"os"
+
+	watchdogworker "tp_distribuidos/watchdog"
+)
+
+func loadConfig() (watchdogworker.WatchdogConfig, error) {
+	momHost := os.Getenv("MOM_HOST")
+	if momHost == "" {
+		return watchdogworker.WatchdogConfig{}, errors.New("MOM_HOST environment variable is required")
+	}
+
+	momPort := 5672
+	if p := os.Getenv("MOM_PORT"); p != "" {
+		if _, err := fmt.Sscanf(p, "%d", &momPort); err != nil {
+			return watchdogworker.WatchdogConfig{}, errors.New("MOM_PORT must be a number")
+		}
+	}
+
+	id := 0
+	if v := os.Getenv("ID"); v != "" {
+		if _, err := fmt.Sscanf(v, "%d", &id); err != nil {
+			return watchdogworker.WatchdogConfig{}, errors.New("ID must be a number")
+		}
+	}
+
+	return watchdogworker.WatchdogConfig{
+		ID:      id,
+		MomHost: momHost,
+		MomPort: momPort,
+	}, nil
+}
+
+func run() int {
+	config, err := loadConfig()
+	if err != nil {
+		slog.Error("loading config", "err", err)
+		return 1
+	}
+
+	w, err := watchdogworker.NewWatchdog(config)
+	if err != nil {
+		slog.Error("initializing heatbeat", "err", err)
+		return 1
+	}
+
+	w.Run()
+	return 0
+}
+
+func main() {
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	})))
+	os.Exit(run())
+}
