192c8a7f94c4cc6938f795be51753c616c7fc123
Ale <alejo.sendra12@gmail.com>
2026-06-24 11:36:51 -0300

    [refactor] changed join q2 msg handling based on worker interface

---
+++ src/join_q2/join_q2/join_q2_restoration.go
@@ -0,0 +1,57 @@
+package join_q2
+
+import (
+	"log/slog"
+	"tp_distribuidos/common/middleware"
+	"tp_distribuidos/common/worker"
+)
+
+const LOGS_UNTIL_CHECKPOINT = 250
+
+// struct usado para el guardado de checkpoints y recuperacion de datos
+type CheckpointData struct {
+	TopByClient      map[int64]map[int]bankEntry `json:"top_by_client"`
+	EofCountByClient map[int64][]string          `json:"eof_count_by_client"`
+}
+
+func (j *JoinQ2) GetCheckpointData() any {
+	return CheckpointData{
+		TopByClient:      j.topByClient,
+		EofCountByClient: j.eofCountByClient,
+	}
+}
+
+func (j *JoinQ2) Restaurate() error {
+	//j.restoring = true
+	// primero restauramos el checkpoint
+	var checkpoint CheckpointData
+
+	thereIsCheckpoint, err := j.datasaver.GetRestaurationCheckpoint(&checkpoint)
+	if err != nil {
+		return err
+	}
+	if thereIsCheckpoint == true {
+		slog.Info("cargando en base a checkpoint")
+		j.topByClient = checkpoint.TopByClient
+		j.eofCountByClient = checkpoint.EofCountByClient
+	}
+
+	var savedDataVar middleware.Message // este tipo de dato es lo unico guardado para este worker
+	var thereIsLogs bool
+
+	for {
+		thereIsLogs, err = j.dataSaver.GetDataFromLogs(&savedDataVar)
+		if err != nil { // habria q modificar para retrys
+			return err
+		}
+		if !thereIsLogs {
+			break
+		}
+		if err := worker.HandleMessageV2(&savedDataVar, j.mssgHandlers); err != nil {
+			return err
+		}
+	}
+
+	//j.restoring = false
+	return nil
+}
